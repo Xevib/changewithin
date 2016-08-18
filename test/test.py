285a7560e8e0e0a40f5471d6701cc4cc80676036
Xevi <xbarnada@gmail.com>
2016-08-18 18:15:56 +0200

    Add test for get_bbox

--- test/test.py
+++ test/test.py
@@ -5,7 +5,12 @@ import unittest
 class ChangesWithinTest(unittest.TestCase):
     def test_get_state(self):
         state = get_state()
-        self.assertGreater(int(state),0)
+        self.assertGreater(int(state), 0)
+
+    def test_get_bbox(self):
+        girona = json.load(open('test/girona.geojson'))
+        bbox = get_bbox(girona['features'][0]['geometry']['coordinates'][0])
+        self.assertEqual(bbox, [2.7831004, 41.9383698, 2.8982394, 42.0297266])
 
 
 if __name__ == '__main__':
