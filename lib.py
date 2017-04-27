4af05d6425c54ca532984862e088da0e81d92388
Xavier Barnada <xbarnada@gmail.com>
2017-04-27 21:18:09 +0200

    Remove unused code

--- lib.py
+++ lib.py
@@ -75,33 +75,6 @@ def point_in_box(x, y, box):
 
     return x > box[0] and x < box[2] and y > box[1] and y < box[3]
 
-
-def point_in_poly(x, y, poly):
-    """
-    To check if a point is inside a polygon
-
-    :param x: X coordiante
-    :param y: Y coordinate
-    :param poly: Polygon as a disc
-    :return: Boolean
-    """
-
-    n = len(poly)
-    inside = False
-    p1x, p1y = poly[0]
-    for i in range(n + 1):
-        p2x, p2y = poly[i % n]
-        if y > min(p1y, p2y):
-            if y <= max(p1y, p2y):
-                if x <= max(p1x, p2x):
-                    if p1y != p2y:
-                        xints = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-                    if p1x == p2x or x <= xints:
-                        inside = not inside
-        p1x, p1y = p2x, p2y
-    return inside
-
-
 def get_extent(gjson):
     """
     Returns the extent of the geojson
