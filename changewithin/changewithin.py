83f3a91c39b3d2df4335ad0fd40547e2cf8610bc
Xavier Barnada <xbarnada@gmail.com>
2017-07-06 23:41:38 +0200

    Fix imports

--- changewithin/changewithin.py
+++ changewithin/changewithin.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import os
 import re
 
@@ -8,7 +9,7 @@ import gettext
 from jinja2 import Environment
 from osconf import config_from_environment
 import osmapi
-from lib import get_osc
+from changewithin.lib import get_osc
 from raven import Client
 
 # Env vars:
