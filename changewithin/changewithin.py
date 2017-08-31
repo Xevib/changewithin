071ed919eb546c4a47d7debfad203e7b127ec905
Xavier Barnada <xbarnada@gmail.com>
2017-08-31 08:06:12 +0200

    Disable relation check

--- changewithin/changewithin.py
+++ changewithin/changewithin.py
@@ -292,37 +292,38 @@ class ChangeHandler(osmium.SimpleHandler):
         # print 'rel:{}'.format(self.num_rel)
         # for member in r.members:
         #    print member
-        if self.rel_in_bbox(rel.members):
-            for tag_name in self.tags.keys():
-                key_re = self.tags[tag_name]["key_re"]
-                value_re = self.tags[tag_name]["value_re"]
-                if self.has_tag(rel.tags, key_re, value_re):
-                    if rel.deleted:
-                        add_rel = True
-                    elif rel.version == 1:
-                        add_rel = True
-                    else:
-                        rel_tags = self.convert_osmium_tags_dict(rel.tags)
-                        add_rel = self.has_tag_changed(rel.id, rel_tags, key_re, rel.version, "relation")
-                    if add_rel:
-                        if tag_name in self.stats:
-                            self.stats[tag_name].add(rel.changeset)
-                        else:
-                            self.stats[tag_name] = [rel.changeset]
-                        if rel.changeset in self.changeset:
-                            if tag_name not in self.changeset[rel.changeset]["rids"]:
-                                self.changeset[rel.changeset]["rids"][tag_name] = []
-                            self.changeset[rel.changeset]["rids"][
-                                tag_name].append(rel.id)
-                        else:
-                            self.changeset[rel.changeset] = {
-                                "changeset": rel.changeset,
-                                "user": rel.user,
-                                "uid": rel.uid,
-                                "nids": {},
-                                "wids": {},
-                                "rids": {tag_name: [rel.id]}
-                            }
+        print ("rel.id {}".format(rel.id))
+        # if not rel.deleted and self.rel_in_bbox(rel):
+        #     for tag_name in self.tags.keys():
+        #         key_re = self.tags[tag_name]["key_re"]
+        #         value_re = self.tags[tag_name]["value_re"]
+        #         if self.has_tag(rel.tags, key_re, value_re):
+        #             if rel.deleted:
+        #                 add_rel = True
+        #             elif rel.version == 1:
+        #                 add_rel = True
+        #             else:
+        #                 rel_tags = self.convert_osmium_tags_dict(rel.tags)
+        #                 add_rel = self.has_tag_changed(rel.id, rel_tags, key_re, rel.version, "relation")
+        #             if add_rel:
+        #                 if tag_name in self.stats:
+        #                     self.stats[tag_name].add(rel.changeset)
+        #                 else:
+        #                     self.stats[tag_name] = [rel.changeset]
+        #                 if rel.changeset in self.changeset:
+        #                     if tag_name not in self.changeset[rel.changeset]["rids"]:
+        #                         self.changeset[rel.changeset]["rids"][tag_name] = []
+        #                     self.changeset[rel.changeset]["rids"][
+        #                         tag_name].append(rel.id)
+        #                 else:
+        #                     self.changeset[rel.changeset] = {
+        #                         "changeset": rel.changeset,
+        #                         "user": rel.user,
+        #                         "uid": rel.uid,
+        #                         "nids": {},
+        #                         "wids": {},
+        #                         "rids": {tag_name: [rel.id]}
+        #                     }
         self.num_rel += 1
 
 
