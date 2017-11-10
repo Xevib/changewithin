f8a8233732da0f5f9917091b8d49ce311fc993e5
xevi <xevi@localhost.localdomain>
2017-11-10 20:38:34 +0100

    Use bigint

--- changewithin/schema.sql
+++ changewithin/schema.sql
@@ -1,4 +1,4 @@
 CREATE EXTENSION POSTGIS;
 CREATE EXTENSION HSTORE;
-CREATE TABLE cache_node (id INTEGER ,version INTEGER ,tags hstore);
+CREATE TABLE cache_node (id BIGINT ,version INTEGER ,tags hstore);
 SELECT AddGeometryColumn ('public','cache_node','geom',4326,'POINT',2, false);
